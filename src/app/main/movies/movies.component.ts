9514a9cca93e2b4042cdf7fba79bf5c3b6c892ff
Hirusha Fernando <heshanhfernando@gmail.com>
2021-11-21 23:53:19 +0530

    Empty data toast

--- src/app/main/movies/movies.component.ts
+++ src/app/main/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie, MOVIE_COLOR } from '../type-definitions';
 import { DatabaseService } from '../../services/database.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-movies',
@@ -13,7 +14,7 @@ export class MoviesComponent implements OnInit {
   color = MOVIE_COLOR;
   loading = false;
 
-  constructor(private databaseService: DatabaseService) { }
+  constructor(private databaseService: DatabaseService, private toastController: ToastController) { }
 
   ngOnInit() {
     this.loading = true;
@@ -25,7 +26,18 @@ export class MoviesComponent implements OnInit {
         this.movies.push(movieObject as Movie);
       });
       this.loading = false;
+      if (this.movies.length === 0) {
+        this.presentToast().then();
+      }
     });
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'No movies in your database',
+      duration: 3000,
+    });
+    await toast.present();
   }
 
 }
